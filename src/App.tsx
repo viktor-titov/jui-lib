78c22c05ffa6dab0570a2d9c8ff9e687d296ba95
Viktor Titov <nulltomato@gmail.com>
2024-05-23 10:08:47 +0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Hello from "./components/Hello";
+import World from "./components/World";
+
+type Props = {
+  [key: string]: unknown;
+};
+
+const App = (props: Props) => {
+  console.log(props);
+  return (
+    <>
+      <div>App</div>
+      <img src="./assets/logo.png" width="100px" height="100px" alt="logo" />
+      <Hello />
+      <World />
+    </>
+  );
+};
+
+export default App;
