02c6d3c071ca9fbad3d8955b363708a9a5cb1eb5
Viktor Titov <nulltomato@gmail.com>
2024-05-23 21:35:37 +0300

    change dependencies

--- dist/index.esm.js
+++ dist/index.esm.js
@@ -1,6 +1,6 @@
 /*!
  *  Name: @viktor-titov/jui-lib
- *  Version: 0.2.1
+ *  Version: 0.2.3
  *  Description: Experiment with config for jaeger-ui components lib
  *  Author: undefined
  *  Homepage: undefined
